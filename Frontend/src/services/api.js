0ce4d847ad1192692c857c1236052d49a005e339
Yehia Medhat <yehiamedhiat@gmail.com>
2026-09-13 21:32:10 +0300

    add url backend 'https://medifind-final-project.onrender.com/api'.

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+  import.meta.env.VITE_API_URL || "https://medifind-final-project.onrender.com/api";
 
 export const getToken = () => localStorage.getItem("token");
 
